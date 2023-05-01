28fd1f64a25cd73e2ae4dec964db73908525cd4a
Ksenia <ksenia.belyaeva.005@mail.ru>
2023-05-01 16:32:29 +0700

    Create Reversed sequence.js

---
+++ Reversed sequence.js
@@ -0,0 +1,15 @@
+/*
+Description:
+
+Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]
+*/
+
+const reverseSeq = n => {
+  let res = []
+  for (let i = n; i > 0; i--) {
+    res.push(i)
+  }
+  return res;
+};

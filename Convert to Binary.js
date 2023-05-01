c2db735fd106a7dd83c94bd67f5921215aa4f745
Ksenia <ksenia.belyaeva.005@mail.ru>
2023-05-01 16:35:17 +0700

    Create Convert to Binary.js

---
+++ Convert to Binary.js
@@ -0,0 +1,11 @@
+/*
+Description:
+Given a non-negative integer n, write a function to_binary/ToBinary which returns that number in a binary format.
+
+Example:
+toBinary(1)  /* should return 1 */
+toBinary(5)  /* should return 101 */
+toBinary(11) /* should return 1011 */
+*/
+
+const toBinary = n => parseInt(n.toString(2));
